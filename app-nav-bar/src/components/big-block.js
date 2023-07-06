aaa7e03fbf89e7f562d51450bd8c20fcd00ff413
Kumar Nitesh <kumar.nitesh@endurance.com>
2023-07-06 14:51:03 -0400

    sync

--- app-nav-bar/src/components/big-block.js
+++ app-nav-bar/src/components/big-block.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function BigBlock() {
-  return <div>External Component : Extracted from Nav Bar MFE</div>;
+  return <div>External Component 123: Extracted from Nav Bar MFE</div>;
 }
